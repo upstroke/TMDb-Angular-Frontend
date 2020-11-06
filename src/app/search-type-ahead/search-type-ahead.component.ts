acf4bf70936697832c74c76cd02d89135c15f5fe
Gregor Arz <gregor.arz@googlemail.com>
2020-11-06 16:42:13 +0100

    fix show/hide on search results

--- src/app/search-type-ahead/search-type-ahead.component.ts
+++ src/app/search-type-ahead/search-type-ahead.component.ts
@@ -9,11 +9,11 @@ import {MovieStoreService} from "../movie-store.service";
 })
 export class SearchTypeAheadComponent implements OnInit {
 
-  resultsMovie :any
-  totalResultsMovie: Number = 0
-  resultsTvShow :any
-  totalResultsTvShow: Number = 0
-  resultsVisible :boolean = false
+  resultsMovie: any;
+  totalResultsMovie = 0;
+  resultsTvShow: any;
+  totalResultsTvShow = 0;
+  resultsVisible = false;
 
 
   constructor(private mss: MovieStoreService) { }
@@ -25,41 +25,41 @@ export class SearchTypeAheadComponent implements OnInit {
       // get all Movies
       this.mss.fetchSearch('movie', $element.target.value)
         .subscribe(res => {
-          this.totalResultsMovie = res.total_results
-          this.resultsMovie = res.results
-          // console.table('result: ',this.resultsMovie)
-        })
+          this.totalResultsMovie = res.total_results;
+          this.resultsMovie = res.results;
+        });
 
       // get all Tv-Shows
       this.mss.fetchSearch('tv', $element.target.value)
         .subscribe(res => {
-          this.totalResultsTvShow = res.total_results
-          this.resultsTvShow = res.results
-          // console.table('result: ',this.resultsTvShow)
-        })
+          this.totalResultsTvShow = res.total_results;
+          this.resultsTvShow = res.results;
+        });
 
-      if(this.totalResultsMovie || this.totalResultsTvShow) {
-        this.resultsVisible = true
+      if (this.totalResultsMovie || this.totalResultsTvShow) {
+        this.resultsVisible = true;
       }else {
-        this.resultsVisible = false
+        this.resultsVisible = false;
       }
+    }else {
+      this.resultsVisible = false;
     }
   }
 
-  toggleResult(action) :void {
-    if(action === 'hide'){this.resultsVisible = false}
+  toggleResult(action): void {
+    if(action === 'hide'){this.resultsVisible = false; }
     if(action === 'show'){
-      if(this.totalResultsMovie || this.totalResultsMovie){
-        this.resultsVisible = true
+      if(this.totalResultsMovie || this.totalResultsTvShow){
+        this.resultsVisible = true;
       }
     }
   }
 
-  groupBy(array, key) :void {
+  groupBy(array, key): void {
     return array.reduce((result, currentValue) => {
       (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue)
       return result;
-    }, {})
+    }, {});
   }
 
 }
